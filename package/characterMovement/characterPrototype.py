92a4780ab2d48501c0a1f901fc8730f0fd260de9
BeRRy <flange0322@gmail.com>
2023-06-04 01:04:41 +0800

    change 285-300's code.

--- package/characterMovement/characterPrototype.py
+++ package/characterMovement/characterPrototype.py
@@ -5,17 +5,17 @@ class common_Skills:
         pass
     def true_Arachnid_reflection(self):
         keyboard.send('w')
-        time.sleep(0.15)
+        time.sleep(0.4)
         print('蜘蛛之鏡')
 
     def solar_Crest(self):
         keyboard.send('e')
-        time.sleep(0.15)
+        time.sleep(0.4)
         print('烈陽印記')
 
     def erda_Shower(self):
         keyboard.send('r')
-        time.sleep(0.15)
+        time.sleep(0.4)
         print('艾爾達斯的降臨')
 
     def rope_Lift(self):
--- package/characterMovement/dualBlade.py
+++ package/characterMovement/dualBlade.py
@@ -7,84 +7,90 @@ class dual_Blade(cp.common_Skills):
         pass
     def blade_Fury(self):
         keyboard.send('v')
-        time.sleep(0.55)
+        time.sleep(0.4)
         print('短刀護佑')
 
     def sudden_Raid(self):
         keyboard.send('s')
-        time.sleep(0.5)
+        time.sleep(0.4)
         print('穢土轉生')
 
     def blades_Of_destiny(self):
         keyboard.send('shift')
-        time.sleep(0.3)
+        time.sleep(0.4)
         print('炎獄修羅斬')
 
     def blade_Tornado(self):
         keyboard.send('d')
-        time.sleep(0.3)
+        time.sleep(0.4)
         print('疾刃颶風')
     
     def final_Cut(self):
         keyboard.send('a')
-        time.sleep(0.3)
+        time.sleep(0.4)
         print('絕殺刃')
     
-    def script4(self):
-        time.sleep(2)
+    def script(self):
         self.frenzy_Totem()
-        time.sleep(1.5)
+        time.sleep(2)
         for i in range(50):
-            print('---------------第' + str(i + 1) + '輪---------------')
-            for i in range(5):
-                self.blade_Fury()
+            print('-------------------第'+str(i)+'輪-------------------')
+            r_str = 'right'
+            l_str = 'left'
+            switch = False
+            for i in range(10):
+                if i == 5:
+                    switch = True
+                    self.rope_Lift()
 
-                keyboard.press('right')
-                keyboard.send('space')
-                time.sleep(0.05)
-                keyboard.send('space')
-                time.sleep(0.2)
-                keyboard.send('space')
-                time.sleep(0.2)
-                keyboard.send('space')
-                time.sleep(0.4)
+                if not switch and i == 0:
+                    keyboard.press(r_str)
+                    time.sleep(0.05)
+                    keyboard.release(r_str)
+                elif switch and i == 5:
+                    keyboard.press(l_str)
+                    time.sleep(0.05)
+                    keyboard.release(l_str)
 
-                
-                if i == 0:
-                    self.blades_Of_destiny()
-                if i == 1:
-                    self.erda_Shower()
-                elif i == 2:
-                    self.blade_Tornado()
+                keyboard.press("space")
+                time.sleep(0.02)
+                keyboard.release("space")
 
-                keyboard.release('right')
-                time.sleep(0.3)
+                keyboard.press("space")
+                time.sleep(0.02)
+                keyboard.release("space")
 
-            self.rope_Lift()
+                keyboard.press("space")
+                time.sleep(0.02)
 
-            for i in range(5):
-                self.blade_Fury()
+                if i % 5 == 0:
+                    if not switch:
+                        self.blade_Fury()
+                    else:
+                        self.erda_Shower()
+                elif i % 5 == 1:
+                    if not switch:
+                        self.blades_Of_destiny()
+                    else:
+                        self.sudden_Raid()
+                        self.blade_Fury()
+                elif i % 5 == 2:
+                    if not switch:
+                        self.blade_Tornado()
+                    else:
+                        self.blade_Fury()
+                elif i % 5 == 3:
+                    if not switch:
+                        self.blade_Fury()
+                    else:
+                        self.blade_Fury()
+                elif i % 5 == 4:
+                    if not switch:
+                        self.blade_Fury()
+                    else:
+                        self.blade_Fury()
 
-                keyboard.press('left')
-                keyboard.send('space')
-                time.sleep(0.05)
-                keyboard.send('space')
-                time.sleep(0.2)
-                keyboard.send('space')
-                time.sleep(0.2)
-                keyboard.send('space')
-                time.sleep(0.4)
+                keyboard.release("space")
 
-                if i == 0:
-                    self.true_Arachnid_reflection()
-                elif i == 1:
-                    self.blades_Of_destiny()
-                elif i == 2:
-                    self.blade_Tornado()
-                elif i == 3:
-                    self.solar_Crest()
-                elif i == 4:
-                    self.sudden_Raid()
-
-                keyboard.release('left')
                 time.sleep(0.5)
+            time.sleep(0.3)
